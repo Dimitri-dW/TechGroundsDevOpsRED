283510c2ffdbb4021895a6d42bcffca27723b056
iyad87 <iyad0tata@gmail.com>
2021-03-07 00:20:42 +0100

    First commit

--- common/utils/errors.ts
+++
@@ -1,9 +0,0 @@
-/** Throw this error to have Lambda function return specific status code and error message in API Gateway response */
-export class StatusCodeError extends Error {
-    readonly statusCode: number;
-
-    constructor(statusCode: number, message: string) {
-        super(message);
-        this.statusCode = statusCode;
-    }
-}
---
+++ y
@@ -0,0 +1,10 @@
+version = 0.1
+[y]
+[y.deploy]
+[y.deploy.parameters]
+stack_name = "sam-app"
+s3_bucket = "aws-sam-cli-managed-default-samclisourcebucket-12397mujwqfmo"
+s3_prefix = "sam-app"
+region = "eu-central-1"
+confirm_changeset = true
+capabilities = "CAPABILITY_IAM"
